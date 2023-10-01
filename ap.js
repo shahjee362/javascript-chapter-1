9e1f755194ea88e69b356685945720f3cb5ca624
shahjee362 <ahmedshah222666@gmail.com>
2023-10-01 15:43:42 -0700

    first commit

---
+++ ap.js
@@ -0,0 +1,13 @@
+alert("Erorr! Please enter a valid password");
+
+
+alert("Welcome to JS Land...  Happy Coding")
+
+
+alert("Welcome to JS land")
+
+
+alert("Happy coding ! prevent this page from creating additional dialouges.")
+
+
+alert("Hello... i can run JS  through my web browser consule")
